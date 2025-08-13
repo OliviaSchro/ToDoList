0ed6214585e7d2537fc2d9bb060909b03aac5362
oliviaschroeder <oliviaschroeder4536@gmail.com>
2025-08-13 17:01:41 -0400

    updated with local storage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,8 @@
-import { Component, signal } from '@angular/core';
+/** LOCAL STORAGE ITEMS
+ * lists, selectedListId, sidebarOpen
+ */
+
+import { Component, OnInit } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
@@ -57,11 +61,41 @@ interface TodoList {
   ],
 })
 
-export class App {
+export class App implements OnInit {
+  sidebarOpen = false;
+  lists: TodoList[] = [];
+  selectedListId: number = 0;
+
+  ngOnInit(): void {
+      const savedLists = this.getLists_strg();
+      const savedSelectedListId = this.getSelectedListId_strg();
+      const savedSidebarOpen = this.getSidebarOpen_strg();
+
+      if(savedLists && savedSelectedListId && savedSidebarOpen) { // there are saved lists! so set global vars here to that
+        this.lists = savedLists;
+        this.selectedListId = savedSelectedListId;
+        this.sidebarOpen = savedSidebarOpen;
+
+      } else { // there are no saved lists so set default
+        this.lists = [
+          { id: 1, name: 'Get Started',
+          todos: [
+              { id: 101, text: 'Make a todo list', completed: false},
+              { id: 102, text: 'add todo list items', completed: false},
+              { id: 103, text: 'Mark them as completed', completed: true},
+            ]
+          }
+        ]; // end of this.lists
+        this.selectedListId = this.lists[0].id;
+        this.sidebarOpen = false;
+
+        this.update_strg();
+      }
+  }
+
   /**--------------------------------------------------------------
    * Setup
   *--------------------------------------------------------------*/
-  sidebarOpen = false;
 
   newListName = new FormControl('', {
     nonNullable: true,
@@ -72,24 +106,9 @@ export class App {
     validators: [Validators.required, Validators.maxLength(150)]
   });
 
-  lists: TodoList[] = [
-    { id: 1, name: 'Get Started',
-    todos: [
-        { id: 101, text: 'Make a todo list', completed: false},
-        { id: 102, text: 'add todo list items', completed: false},
-        { id: 103, text: 'Mark them as completed', completed: true},
-      ]
-    }
-  ];
-
-  selectedListId: number = this.lists[0].id;
-
   /**--------------------------------------------------------------
    * UI ACTIONS & HELPERS
    *--------------------------------------------------------------*/
-  toggleSidebar() {this.sidebarOpen = !this.sidebarOpen;}
-  selectList(id: number) {this.selectedListId = id;}
-
   get selectedList(): TodoList | undefined {
     return this.lists.find(l => l.id === this.selectedListId);
   }
@@ -100,6 +119,15 @@ export class App {
   /**--------------------------------------------------------------
    * FUNCTIONS
    *--------------------------------------------------------------*/
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+    this.update_strg();
+  }
+  selectList(id: number) {
+    this.selectedListId = id;
+    this.update_strg();
+  }
+
 
   addList() {
     const name = this.newListName.value.trim();
@@ -108,6 +136,9 @@ export class App {
     const newList: TodoList = { id: Date.now(), name, todos: [] };
     this.lists = [...this.lists, newList]; // add it to our lists
     this.selectedListId = newList.id;
+
+    this.update_strg(); // UPDATE LOCAL STORAGE
+
     this.newListName.reset('');
   }
 
@@ -125,6 +156,8 @@ export class App {
       const newIdx = Math.max(0, idx-1); //making sure valid
       this.selectedListId = this.lists[newIdx].id;
     }
+
+    this.update_strg(); // UPDATE LOCAL STORAGE
   }
 
   // ! HAVENT IMPLEMENTED YET
@@ -132,6 +165,8 @@ export class App {
     const list = this.lists.find(l => l.id === id);
     if (!list) return;
     list.name = name.trim() || list.name;
+
+    this.update_strg(); // UPDATE LOCAL STORAGE
   }
 
   addTodo() {
@@ -142,6 +177,9 @@ export class App {
       ...list.todos,
       { id: Date.now(), text, completed: false }
     ];
+
+    this.update_strg(); // UPDATE LOCAL STORAGE
+
     this.newTodo.reset('');
   }
 
@@ -151,11 +189,45 @@ export class App {
     list.todos = list.todos.map(t =>
       t.id === todoId ? {...t, completed: !t.completed } : t
     );
+
+    this.update_strg(); // UPDATE LOCAL STORAGE
   }
 
   deleteTodo(todoId: number) {
     const list = this.selectedList;
     if (!list) return;
     list.todos = list.todos.filter(t => t.id !== todoId);
+
+    this.update_strg(); // UPDATE LOCAL STORAGE
+  }
+
+
+
+  // STORAGE --- update when you set the lists and savedListid vars
+  update_strg() {
+    this.setLists_strg(this.lists);
+    this.setSelectedListId_strg(this.selectedListId);
+    this.setSidebarOpen(this.sidebarOpen);
+  } 
+  getSelectedListId_strg(): number | null {
+    const id = localStorage.getItem('selectedListId');
+    return id ? parseInt(id,10) : null;
+  }
+  setSelectedListId_strg(id: number): void {
+    localStorage.setItem('selectedListId', id.toString());
+  }
+  getLists_strg(): TodoList[] | null {
+    const list = localStorage.getItem('lists');
+    return list ? JSON.parse(list) : null;
+  }
+  setLists_strg(list: TodoList[]): void {
+    localStorage.setItem('lists', JSON.stringify(list));
+  }
+  getSidebarOpen_strg(): boolean | null {
+    const sb = localStorage.getItem('sidebarOpen');
+    return sb !== null ? JSON.parse(sb) : null;
+  }
+  setSidebarOpen(sb: boolean): void {
+    localStorage.setItem('sidebarOpen', JSON.stringify(sb));
   }
 }
